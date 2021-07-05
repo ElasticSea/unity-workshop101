ae22210a980b68ef40257a0e7aed0508b21b2054
ElasticSea <info@elasticsea.com>
2021-07-05 17:56:12 +0200

    Rotate tank with horizontal input
    Samples horizontal input from keyboard, rotate tank around it's axis left and right with left and right arrow keys. Modify the rotateSpeed in inspector to change how fast the tank rotates

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -5,6 +5,9 @@ public class Tank : MonoBehaviour
     [Tooltip("Determines how fast is the tank going to move forward and backward")]
     [SerializeField] private float moveSpeed = 5;
     
+    [Tooltip("Determines how fast tank rotates left and right around it's axis")]
+    [SerializeField] private float rotateSpeed = 360;
+    
     void Update()
     {
         // Samples input that corresponds to vertical axi, up down arrows on keyboard
@@ -13,5 +16,10 @@ public class Tank : MonoBehaviour
         // Move in the direction that tank is facing
         var moveDistance = Time.deltaTime * moveSpeed * moveInput;
         transform.position += moveDistance * transform.forward;
+
+        // Rotate tank around it's up axis
+        var rotateInput = Input.GetAxis("Horizontal");
+        var rotateDistance = new Vector3(0, Time.deltaTime * rotateSpeed * rotateInput, 0);
+        transform.Rotate(rotateDistance, Space.Self);
     }
 }
