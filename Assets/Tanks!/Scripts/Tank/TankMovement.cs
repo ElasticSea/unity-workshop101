e1461e761948cbac4d3bd10fc07ebec4fa0a1902
Your Name <you@example.com>
2021-08-24 17:36:13 +0200

    Project1

--- Assets/Tanks!/Scripts/Tank/TankMovement.cs
+++ Assets/Tanks!/Scripts/Tank/TankMovement.cs
@@ -10,7 +10,7 @@ public class TankMovement : MonoBehaviour
     public AudioClip m_EngineDriving;      
     public float m_PitchRange = 0.2f;
 
-    /*
+    
     private string m_MovementAxisName;     
     private string m_TurnAxisName;         
     private Rigidbody m_Rigidbody;         
@@ -46,7 +46,7 @@ public class TankMovement : MonoBehaviour
 
         m_OriginalPitch = m_MovementAudio.pitch;
     }
-    */
+    
 
     private void Update()
     {
